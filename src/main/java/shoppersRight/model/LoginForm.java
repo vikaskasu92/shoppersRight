8c4ea84b10bb5086d20bec12738b255182d56c10
vikaskasu92 <vikaskasu92@gmail.com>
2018-11-18 21:34:49 -0500

    Create LoginForm.java

---
+++ src/main/java/shoppersRight/model/LoginForm.java
@@ -0,0 +1,29 @@
+package shoppersRight.model;
+
+public class LoginForm {
+
+	private String username;
+	private String password;
+	private String userType;
+	
+	
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
